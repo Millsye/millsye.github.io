3e9c659e41697a376d784ea7c39e5f73764172cb
Miles Ericson <miles.ericson@rcis.com>
2025-06-11 15:08:05 -0500

    added another project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import dronedelivery from '../assets/images/droneproject.jpeg';
 import sportsmodel from '../assets/images/sportsmodel.jpeg';
+import cybersecurity from '../assets/images/cybersecurity.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -12,6 +13,11 @@ function Project() {
                 <a href="https://youtu.be/ACWOi3qdMEs" target="_blank" rel="noreferrer"><img src={dronedelivery} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://youtu.be/ACWOi3qdMEs" target="_blank" rel="noreferrer"><h2>Drone Delivery Simulation</h2></a>
                 <p>Created extensions and improvements to a Drone Delivery System, implementing a Singleton and State design patterns to improve the simulations efficiency, scalability, and usability.</p>
+            </div>
+            <div className="project">
+                <a href="https://coursera.org/share/609a2c917e2a3e102ae8e553629875d1" target="_blank" rel="noreferrer"><img src={cybersecurity} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://coursera.org/share/609a2c917e2a3e102ae8e553629875d1" target="_blank" rel="noreferrer"><h2>Google Cybersecurity Certificate</h2></a>
+                <p>Learned about the importance of cybersecurity and common practices used by Google and other organizations. Used Python, Linux, and SQL in hands-on activites as well as utilized SIEM tools to protect from mock attacks.</p>
             </div>
             <div className="project">
                 <a href="https://docs.google.com/spreadsheets/d/16ixAojepLrPMrDPaandZI8Oz1tpz5Y4r3nFFF-lNFGA/edit?usp=sharing" target="_blank" rel="noreferrer"><img src={sportsmodel} className="zoom" alt="thumbnail" width="100%"/></a>
