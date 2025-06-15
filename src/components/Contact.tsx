df24ebd045d1db11c0891ab6caae420b3471c4d8
Miles Ericson <miles_ericson@icloud.com>
2025-06-14 22:58:28 -0500

    added portfolio changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,111 +1,63 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import '../assets/styles/Contact.scss';
-// import emailjs from '@emailjs/browser';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
+import gophers from '../assets/images/gophers.jpeg';
+import outdoors from '../assets/images/outdoors.jpeg';
+import resume from '../assets/images/resume.png';
 
 function Contact() {
-
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-
-  const [nameError, setNameError] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [messageError, setMessageError] = useState<boolean>(false);
-
-  const form = useRef();
-
-  const sendEmail = (e: any) => {
-    e.preventDefault();
-
-    setNameError(name === '');
-    setEmailError(email === '');
-    setMessageError(message === '');
-
-    /* Uncomment below if you want to enable the emailJS */
-
-    // if (name !== '' && email !== '' && message !== '') {
-    //   var templateParams = {
-    //     name: name,
-    //     email: email,
-    //     message: message
-    //   };
-
-    //   console.log(templateParams);
-    //   emailjs.send('service_id', 'template_id', templateParams, 'api_key').then(
-    //     (response) => {
-    //       console.log('SUCCESS!', response.status, response.text);
-    //     },
-    //     (error) => {
-    //       console.log('FAILED...', error);
-    //     },
-    //   );
-    //   setName('');
-    //   setEmail('');
-    //   setMessage('');
-    // }
-  };
-
   return (
     <div id="contact">
       <div className="items-container">
         <div className="contact_wrapper">
-          <h1>Contact Me</h1>
-          <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
-          <Box
-            ref={form}
-            component="form"
-            noValidate
-            autoComplete="off"
-            className='contact-form'
-          >
-            <div className='form-flex'>
-              <TextField
-                required
-                id="outlined-required"
-                label="Your Name"
-                placeholder="What's your name?"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                error={nameError}
-                helperText={nameError ? "Please enter your name" : ""}
-              />
-              <TextField
-                required
-                id="outlined-required"
-                label="Email / Phone"
-                placeholder="How can I reach you?"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                error={emailError}
-                helperText={emailError ? "Please enter your email or phone number" : ""}
-              />
+          <h1>About Me</h1>
+          <Box className="about-me-content">
+            <div className="about-me-section alternating-section">
+              <div className="text-content">
+                <h2>Who I Am</h2>
+                <p>
+                  I am a computer science student at the University of Minnesota Twin Cities (RTB!). I enjoy going to watching and attending many different sporting events,
+                  as well as playing video games, LEGOs, and the outdoors. I am passionate about technology and love to learn new things.
+                </p>
+              </div>
+              <div className="image-content">
+                <img src={gophers} alt="Who I Am" />
+              </div>
             </div>
-            <TextField
-              required
-              id="outlined-multiline-static"
-              label="Message"
-              placeholder="Send me any inquiries or questions"
-              multiline
-              rows={10}
-              className="body-form"
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-              error={messageError}
-              helperText={messageError ? "Please enter the message" : ""}
-            />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={sendEmail}>
-              Send
-            </Button>
+            <div className="about-me-section alternating-section">
+              <div className="image-content">
+                <img src={outdoors} alt="My Interests" />
+              </div>
+              <div className="text-content">
+                <h2>My Interests</h2>
+                <p>
+                  I love gardening, exercising, sports, and coding. I enjoy full stack development with an emphasis on the backend.
+                  As a newer developer, I am always eager to learn and explore new technologies and frameworks.
+                </p>
+              </div>
+            </div>
+          </Box>
+          <Box className="contact-content">
+            <div className="resume-section">
+              <h2>Resume</h2>
+              <Button
+                variant="contained"
+                href={resume}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View My Resume
+              </Button>
+            </div>
+            <div className="text-content">
+              <h2>Contact Info</h2>
+              <p>
+                Email: miles_ericson@icloud.common<br />
+                School Email: erics184@umn.edu<br />
+                Phone: 763-760-0401<br />
+              </p>
+            </div>
           </Box>
         </div>
       </div>
